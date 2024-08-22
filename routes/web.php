4c717f762951972fa4350eea90fb929b2335c249
Stefan Pertschi <stefan.pertschi@gfn.de>
2024-08-22 15:24:16 +0200

    WE-Stuff deleted

--- routes/web.php
+++ routes/web.php
@@ -13,28 +13,3 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
-
-// WE 
-
-// Route::get('/hallowelt', function () {
-//     return "hallowelt 🦊";
-// });
-// 
-// Route::get('/user/{id?}', function ($id = null) {
-//     return "Der User hat die folgende Id:".$id;
-// });
-// 
-// Route::get('/vorname/{vorname?}/nachname/{nachname?}', function ($vorname = null, $nachname = null) {
-//     return "Hallo ".$vorname." ".$nachname . "🏴";
-// });
-// 
-// 
-// Route::get('/user/setting', function () {
-//     return "einstellungen";
-// })->name("settings");
-// 
-// Route::get('/morin', function () {
-//     return 'Das Denken, welches vereinfacht, 
-//     ist zur Barbarei der Wissenschaft geworden. 
-//     Dies ist die spezifische Barbarei unserer Zivilisation';
-// })->name('morin');
