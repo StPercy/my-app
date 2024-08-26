c67f12994ca12b95053ebc21969f2b1ea166c560
Stefan Pertschi <stefan.pertschi@gfn.de>
2024-08-26 16:43:36 +0200

    day16 minute 11:19

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,26 @@ Route::get('/', function () {
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->paginate(3);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
+Route::get('/jobs/create', function () {
+    //dd('create');
+    return view('jobs.create');
+});
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     //dd($job);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
+
+Route::post('/jobs', function () {
+    dd('hello from the post request');
+});
+
 
 Route::get('/contact', function () {
     return view('contact');
