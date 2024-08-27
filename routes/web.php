d9fd5990dc08213541b83b5cabd379b2b1814b35
Stefan Pertschi <stefan.pertschi@gfn.de>
2024-08-27 17:46:58 +0200

    finished day19_2, sorted routes, cleaned up web.php

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
-// Route::controller(JobController::class)->group(function () {
-//     Route::get('/jobs',  'index');
-//     Route::get('/jobs/create',  'create');
-//     Route::get('/jobs/{job}', 'show');
-//     Route::post('/jobs', 'store');
-//     Route::get('/jobs/{job}/edit', 'edit');
-//     Route::patch('/jobs/{job}', 'update');
-//     Route::delete('/jobs/{job}', 'destroy');
-// });
-
-Route::resource('jobs', JobController::class, [
-    //'except' => ['edit']
-    //'only' => ['index', 'edit', 'show', 'create', 'store']
-]);
+Route::resource('jobs', JobController::class);
